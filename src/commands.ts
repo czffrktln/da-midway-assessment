a5cae67119a619ddba405661cf270ce0525cca2b
Katalin Czifferi <katica@Katalins-Air.lan>
2024-10-21 16:48:27 +0200

    show recipe details

--- src/commands.ts
+++ src/commands.ts
@@ -14,3 +14,19 @@ export async function list(store: Store<RecipeType[]>, args: string[]) {
     console.log(`Error: The list command should not have any argument.`);
   }
 }
+
+export async function details(store: Store<RecipeType[]>, args: string[]) {
+  const [id] = args;
+  if (isNaN(Number(id)) || args.length > 1) {
+    console.log('You should give only 1 numeric argument');
+  } else {
+    const recipe = new Recipe(store);
+    const recipes = await recipe.readAll();
+    const findRecipe = recipes.find((recipe) => recipe.id === Number(id))
+    if (findRecipe) {
+      console.log(`ID: ${findRecipe.id} \nName: ${findRecipe.name}`);
+    } else {
+      console.log("There is no recipe with this ID");
+    }
+  }
+}
